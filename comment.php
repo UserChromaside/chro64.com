57c5e205af15d2d19a3f778157e59ed488d02896
UserChromaside <144497379+UserChromaside@users.noreply.github.com>
2024-05-16 20:23:01 -0700

    Update comment.php

--- comment.php
+++ comment.php
@@ -6,7 +6,7 @@
 	<meta property="og:locale" content="en_US">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Comment</title>
-	<link rel="stylesheet" href="/main.css">
+	<link rel="stylesheet" href="comment.css">
 	</head>
 	
 	<body bgcolor="#ffffff" text="#000000">
