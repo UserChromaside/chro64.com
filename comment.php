92489f856eb2be4bb63bbc204efc2e1b4cee1ab6
UserChromaside <144497379+UserChromaside@users.noreply.github.com>
2024-05-16 20:22:14 -0700

    Create comment.php

---
+++ comment.php
@@ -0,0 +1,13 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html lang="en">
+	<head>
+	<meta charset="utf-8">
+	<meta property="og:locale" content="en_US">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Comment</title>
+	<link rel="stylesheet" href="/main.css">
+	</head>
+	
+	<body bgcolor="#ffffff" text="#000000">
+	<p><font face="arial, sans-serif" size="2">You must type in a name.</font></p></body></html>
